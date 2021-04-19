fde20025de15568c545d87072e80d49fad4962b4
Starlight Romero <starlightromero@protonmail.com>
2021-04-19 11:59:06 -0700

    [update] added Update method to dictionary

--- dictionary.go
+++ dictionary.go
@@ -1,20 +1,24 @@
 package main
 
-import "errors"
-
 const (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+	// ErrNotFound means the definition could not be found for the given word
+	ErrNotFound = DictionaryErr("could not find the word you were looking for")
+
+	// ErrWordExists means you are trying to add a word that is already known
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
-type Dictionary map[string]string
-
+// DictionaryErr are errors that can happen when interacting with the dictionary
 type DictionaryErr string
 
 func (e DictionaryErr) Error() string {
-    return string(e)
+	return string(e)
 }
 
+// Dictionary stores definitions to words
+type Dictionary map[string]string
+
+// Search find a word in the dictionary
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
@@ -24,9 +28,10 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
+// Add inserts a word and definition into the dictionary
 func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
-	
+
 	switch err {
 	case ErrNotFound:
 		d[word] = definition
@@ -37,4 +42,9 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 	d[word] = definition
 	return nil
+}
+
+// Update changes the definition of a given word
+func (d Dictionary) Update(word, definition string) {
+	d[word] = definition
 }
